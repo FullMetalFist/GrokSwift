bb5f1e3787e1f784b0a34cb90e6e6b6be4588019
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-12 15:55:33 -0500

    New webservice call using Alamofire

--- GrokRESTAPI/ViewController.swift
+++ GrokRESTAPI/ViewController.swift
@@ -13,122 +13,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        request3()
+        getRequestAlamo()
     }
     
-    func request3() {
-        // DELETE
-        let todoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
-        
-        guard let todoURL = URL(string: todoEndpoint) else {
-            print("no URL")
-            return
-        }
-        
-        var todoURLRequest = URLRequest(url: todoURL)
-        todoURLRequest.httpMethod = "DELETE"
-        
-        let session = URLSession.shared
-        let task = session.dataTask(with: todoURLRequest) { (data, response, error) in
-            
-            guard let _ = data else {
-                print("error calling DELETE on /todos/1")
-                return
-            }
-            print("DELETE okay")
-        }
-        task.resume()
-    }
-    
-    func request2() {
-        // POST
-        let todoEndpoint = "https://jsonplaceholder.typicode.com/todos"
-        guard let todoURL = URL(string: todoEndpoint) else {
-            print("can't create url")
-            return
-        }
-        var todoURLRequest = URLRequest(url: todoURL)
-        todoURLRequest.httpMethod = "POST"
-        let addTodo: [String: Any] = ["title": "First todo", "completed": false, "userID": 1]
-        let jsonTodo: Data
-        do {
-            jsonTodo = try JSONSerialization.data(withJSONObject: addTodo, options: [])
-            todoURLRequest.httpBody = jsonTodo
-        } catch {
-            print("error: cannot create JSON from todo")
-            return
-        }
-        let session = URLSession.shared
-        let task = session.dataTask(with: todoURLRequest) { (data, response, error) in
-            guard error == nil else {
-                print("error calling POST on /todos/1")
-                return
-            }
-            guard let responseData = data else {
-                print("error: did not receive data")
-                return
-            }
-            
-            do {
-                guard let receivedTodo = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any] else {
-                    print("could not get JSON from responseData as dictionary")
-                    return
-                }
-                print("the todo is: " + receivedTodo.description)
-                guard let todoID = receivedTodo["id"] as? Int else {
-                    print("could not get todoID as Int from JSON")
-                    return
-                }
-                print("The ID is: \(todoID)")
-            } catch {
-                print("error parsing response from POST on /todos")
-            }
-        }
-        task.resume()
-    }
-    
-    func request1() {
-        // GET
+    func getRequestAlamo() {
         let todoEndpoint: String = "https://jsonplaceholder.typicode.com/todos/1"
-        
-        guard let url = URL(string: todoEndpoint) else { return }
-        
-        let urlRequest = URLRequest(url: url)
-        
-        let session = URLSession.shared
-        let task = session.dataTask(with: urlRequest, completionHandler: {
-            data, response, error in
-            
-            guard error == nil else {
-                print("error calling GET on /todos/1")
-                print(error!)
+        Alamofire.request(todoEndpoint).responseJSON { response in
+            guard let json = response.result.value as? [String: Any] else {
+                print("didn't get todo object as JSON from API")
+                print("Error: \(String(describing: response.result.error))")
                 return
             }
-            guard let responseData = data else {
-                print("Error: did not receive data")
-                return
-            }
-            // parse result as JSON
-            do {
-                guard let todo = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any] else {
-                    print("error trying to convert data to JSON")
-                    return
+            print(json)
+        }
+            .responseString { response in
+                if let error = response.result.error {
+                    print(error)
                 }
-                // print todo
-                print("The todo is: " + todo.description)
-                
-                guard let todoTitle = todo["title"] as? String else {
-                    print("error converting title")
-                    return
+                if let value = response.result.value {
+                    print(value)
                 }
-                
-                print("the title is: " + todoTitle)
-            } catch {
-                print("error trying to convert data to JSON")
-                return
-            }
-        })
-        task.resume()
+        }
     }
 }
 
