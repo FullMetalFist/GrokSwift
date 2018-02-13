be28e502ec78595dab7afe1a61d9db2525f2986c
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-13 12:36:07 -0500

    Todo extension

---
+++ GrokRESTAPI/Todo+Networking.swift
@@ -0,0 +1,9 @@
+//
+//  Todo+Networking.swift
+//  GrokRESTAPI
+//
+//  Created by Michael Vilabrera on 2/13/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import Foundation
