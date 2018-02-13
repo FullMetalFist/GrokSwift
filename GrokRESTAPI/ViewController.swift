0c9be8f8937bf97eec09306d10cb7a4b4894b70d
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-13 10:38:36 -0500

    Alamofire DELETE method

--- GrokRESTAPI/ViewController.swift
+++ GrokRESTAPI/ViewController.swift
@@ -19,20 +19,62 @@ class ViewController: UIViewController {
     func getRequestAlamo() {
         let todoEndpoint: String = "https://jsonplaceholder.typicode.com/todos/1"
         Alamofire.request(todoEndpoint).responseJSON { response in
+            
+            guard response.result.error == nil else {
+                print("error calling GET on /todos/1")
+                print(response.result.error!)
+                return
+            }
+            
             guard let json = response.result.value as? [String: Any] else {
                 print("didn't get todo object as JSON from API")
                 print("Error: \(String(describing: response.result.error))")
                 return
             }
-            print(json)
+            
+            guard let todoTitle = json["title"] as? String else {
+                print("could not get todo title from JSON")
+                return
+            }
+            print("the title is: " + todoTitle)
         }
-            .responseString { response in
-                if let error = response.result.error {
-                    print(error)
-                }
-                if let value = response.result.value {
-                    print(value)
-                }
+    }
+    
+    func postRequestAlamo() {
+        let todoEndpoint: String = "https://jsonplaceholder.typicode.com/todos"
+        
+        let newTodo: [String: Any] = ["title": "First Alamo Post", "completed": 0, "userId": 1]
+        Alamofire.request(todoEndpoint, method: .post, parameters: newTodo, encoding: JSONEncoding.default).responseJSON { response in
+            
+            guard response.result.error == nil else {
+                print("error calling POST on /todos/1")
+                print(response.result.error!)
+                return
+            }
+            
+            guard let json = response.result.value as? [String: Any] else {
+                print("didn't get todo object as JSON from API")
+                print("Error: \(String(describing: response.result.error))")
+                return
+            }
+            guard let todoTitle = json["title"] as? String else {
+                print("could not get todo title")
+                return
+            }
+            print("the title is: \(todoTitle)")
+        }
+    }
+    
+    func deleteRequestAlamo() {
+        let todoEndpoint: String = "https://jsonplaceholder.typicode.com/todos/1"
+        
+        Alamofire.request(todoEndpoint, method: .delete).responseJSON { response in
+            guard response.result.error == nil else {
+                print("error calling DELETE on /todos/1")
+                print(response.result.error!)
+                return
+            }
+            print("DELETE ok")
         }
     }
 }
